ab915925424f0c9d1578de1f35b6bb12bb3eb926
Sviatoslav Chapcha <whitetark@gmail.com>
2023-05-02 00:14:03 +0300

    optimize 1st task

--- lab07/src/Main.java
+++ lab07/src/Main.java
@@ -16,28 +16,44 @@ public class Main {
         int size = MPI.COMM_WORLD.Size();
 
         if(rank == MASTER){
+            System.out.println("Started with " + size + " tasks");
             matrixA = Methods.generateMatrix(matrixSize);
             matrixB = Methods.generateMatrix(matrixSize);
             startTime = System.currentTimeMillis();
         }
 
-        int count = matrixA.length/size;
+        int work = matrixSize / size;
+        int extra = matrixSize % size;
 
-        double[][] blockA = new double[count][matrixSize];
-        MPI.COMM_WORLD.Scatter(matrixA, 0, count, MPI.OBJECT, blockA, 0, count, MPI.OBJECT, 0);
+        int[] sendcount = new int[size];
+        int[] displace = new int[size];
+
+        displace[0] = 0;
+        for(int i = 1; i < size; i++){
+            sendcount[i-1] = work;
+            if(i == size - 1){
+                sendcount[i-1] += extra;
+            }
+            displace[i] = sendcount[i-1] + displace[i-1];
+        }
+        sendcount[size-1] = work;
+
+        int currentWork = sendcount[rank];
+
+        double[][] blockA = new double[currentWork][matrixSize];
+        MPI.COMM_WORLD.Scatterv(matrixA, 0, sendcount, displace, MPI.OBJECT, blockA, 0, currentWork, MPI.OBJECT, 0);
         MPI.COMM_WORLD.Bcast(matrixB,0, matrixB.length, MPI.OBJECT,0);
 
         MPI.COMM_WORLD.Barrier();
         double[][] subResult = Methods.multiply(blockA, matrixB);
 
         double[][] result = new double[matrixSize][matrixSize];
-        MPI.COMM_WORLD.Gather(subResult,0, subResult.length, MPI.OBJECT, result,0, subResult.length, MPI.OBJECT,0);
+        MPI.COMM_WORLD.Gatherv(subResult,0, subResult.length, MPI.OBJECT, result,0, sendcount, displace, MPI.OBJECT,0);
 
         if(rank == MASTER){
             endTime = System.currentTimeMillis();
-            Methods.print(result);
-            System.out.println("time " + (endTime-startTime));
-            System.out.println(result.length);
+            //Methods.print(result);
+            System.out.println("Time: " + (endTime-startTime));
         }
         MPI.Finalize();
     }
